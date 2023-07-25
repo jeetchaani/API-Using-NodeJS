de94eb0f2261342098aea2eebfb5d947258ef830
Jeet Chaani <jeetchaani23@gmail.com>
2023-07-25 20:59:00 +0530

    login register api

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,73 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
 
-const register = (req,res) =>{
-    return res.status(201).json({ msg: "Working" });
+
+const register = async (req,res) =>{
+        const { name, email, password } = req.body;
+        //validate user input
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+              return res.status(400).json({ message : 'Validation Errors',
+                errors: errors.array() });
+        }
+
+           try {
+                  let user = await User.findOne({ email });
+                  if(user){
+                    return res.status(400).json({ message : 'User Already Exist' });
+                  }  
+                const salt = await bcrypt.genSalt(10);
+                const hassedPassword = await bcrypt.hash(password, salt);
+               user = new User({
+                name,
+                email,
+                password :hassedPassword
+               });
+              await user.save();
+              res.status(201).json({
+                  message: 'User Created',
+                  user: user
+              });
+           } catch (error) {
+                  res.status(500).json({
+                       message :'Internal Server Error'
+                  });
+           }
+    
 }
 
-module.exports = { register };
+const login = async (req, res) =>{
+      const { email, password } = req.body;
+
+      //validate the errors
+     const errors = validationResult(req);
+       if(!errors.isEmpty()){
+        return res.status(400).json({ message: 'Validation Error',
+          errors: errors.array()
+         });
+       }
+       
+       try {
+            const user = await User.findOne({ email });
+            if(!user) {
+              return res.status(400).json({ message: 'Invalid Credentials' });
+            }
+           const isPasswordValid = await bcrypt.compare(password, user.password);
+             if(!isPasswordValid){
+                return res.status(400).json({ message: 'Invalid Password' });
+             }
+           const token = jwt.sign({ id:user._id }, process.env.JWT_SECRET, {
+            expiresIn : '1d'
+           });
+           return res.status(200).json({ message : 'Login',
+            token: token });
+            
+       } catch (error) {
+          console.error(err);
+        res.status(500).json({ message: 'Internal Server Error' });
+       }
+}
+
+module.exports = { register, login };
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 const authController = require('../controllers/authController');
 
 //call url
-router.get('/register',authController.register);
+router.post('/register', [
+     check('name', 'Name is Required').notEmpty(),
+     check('email', 'Email is Required').notEmpty(),
+     check('password', 'Password must be 6 characters Long').isLength({
+        min:6,
+     }),
+], authController.register);
+
+router.post('/login',[
+    check('email', 'Email is Required').notEmpty(),
+    check('password', 'Password is Required').notEmpty()
+], authController.login);
 
 module.exports = router;
